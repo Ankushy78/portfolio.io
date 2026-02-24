861898fd161a1c57fc6e716e23a70e8d1d2ee1ec
Ak-WEBDEV <129375288+Ankushy78@users.noreply.github.com>
2026-02-24 20:32:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["UX-Design", "UI-Design",  "Wireframing", "Prototyping","Design Systems"],
+    strings: ["AI - Engineer", "UI-Design",  "Backend", "Prototyping","Deployment"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
@@ -167,4 +167,5 @@ srtop.reveal('.experience .timeline .container', { interval: 400 });
 /* SCROLL CONTACT */
 srtop.reveal('.contact .container', { delay: 400 });
 srtop.reveal('.contact .container .form-group', { delay: 400 });
+
 
